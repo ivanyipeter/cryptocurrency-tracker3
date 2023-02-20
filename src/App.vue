c5d9c1939435df67f0dfedd68bd32de454f39bc2
Peter Ivanyi <ivanyipeter@gmail.com>
2023-02-20 18:28:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,174 @@
+<template>
+    <CoinDetailsModal v-if="showCoinModal" :coinId="coinId" :coinSymbol="coinSymbol" :coinHttp="coinHttp"  @close="closeCoinDetailsModal">
+    </CoinDetailsModal>
+
+    <h1>Markets</h1>
+    <input v-model="text" placeholder="Search..">
+    <br>
+    <table>
+        <thead>
+        <tr>
+            <th style="width: 5%">#</th>
+            <th style="width: 5%"></th>
+            <th style="width: 10%"></th>
+            <th style="width: 10%">Name</th>
+            <th>Current Price</th>
+            <th>Price Change</th>
+            <th>Market Cap</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tr v-for="(currency,index) in filteredList" :key="index">
+            <td>{{index}}</td>
+            <td><img class="coinImg" :src="currency.image"></td>
+            <td>{{currency.symbol.toUpperCase()}}</td>
+            <td>{{currency.name}}</td>
+            <td>{{currency.current_price}}</td>
+            <td :class="priceChangeColor(currency.price_change_24h)">{{currency.price_change_24h}}</td>
+            <td>{{currency.market_cap}}</td>
+            <td>
+                <button @click="showDetails(currency.id, currency.symbol, currency.image)">Detail</button>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+
+    import CoinDetailsModal from "./components/CoinDetailsModal";
+
+    export default {
+        name: 'App',
+        components: {CoinDetailsModal},
+        data() {
+            return {
+                currencyList: [],
+                text: "",
+                input: null,
+                coinId: null,
+                coinSymbol: null,
+                coinHttp: null,
+                showCoinModal: false
+            }
+        },
+        methods: {
+            async getCoins() {
+                fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+                    .then(res => res.json())
+                    .then(data => {
+                        this.currencyList = data;
+                    }).catch(e => {
+                    alert(e)
+                });
+            },
+            showDetails(coinId, coinSymbol, coinHttp) {
+                this.coinId = coinId;
+                this.coinSymbol = coinSymbol;
+                this.coinHttp = coinHttp;
+                this.showCoinModal = true;
+            },
+            priceChangeColor(price) {
+                return price > 0 ? 'price-up' : 'price-down'
+            },
+
+            closeCoinDetailsModal() {
+                this.showCoinModal = false;
+            }
+        }
+        ,
+        computed: {
+            filteredList() {
+                return this.currencyList.filter(f => f.name.toUpperCase().includes(this.text.toUpperCase()) || f.symbol.toUpperCase().includes(this.text.toUpperCase()));
+            }
+        },
+        mounted() {
+            this.getCoins();
+        },
+        created() {
+        }
+    }
+</script>
+
+<style>
+    body {
+        background: #00003B;
+    }
+
+    #app {
+        /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+        font-family: "Segoe UI", serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        /*text-align: center;*/
+        /*margin-top: 100px;*/
+        background: #00003B;
+    }
+</style>
+
+<style scoped>
+    table {
+        color: white;
+        border-collapse: collapse;
+        width: 60%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    td, th {
+        border-bottom: solid black 1px;
+        text-align: left;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    .coinImg {
+        height: 30px;
+    }
+
+    /*thead {*/
+    /*    font-size: 20px;*/
+    /*}*/
+
+    h1 {
+        color: white;
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+    input {
+        display: block;
+        color: black;
+        margin-left: auto;
+        margin-right: auto;
+        width: 60%;
+        background: #303655;
+        border: 1px solid black;
+        border-radius: 3px;
+        padding: 10px;
+    }
+
+    .price-down {
+        color: red;
+    }
+
+    .price-up {
+        color: greenyellow;
+    }
+
+    ::placeholder {
+        color: white;
+    }
+
+    button {
+        padding: 5px 10px 5px 10px;
+        background: #00003B;
+        border: none;
+        color: yellow;
+    }
+
+    button:hover {
+        color: greenyellow;
+    }
+
+
+</style>
---
+++ src/components/CoinDetailsModal.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+                <div class="close">
+                    <span @click="closeModal">x</span>
+                </div>
+                <div class="header">
+                    <img v-if="coinHttp" class="item" :src="coinHttp.replace('large','small')">
+                    <p class="item">{{coinId.toUpperCase()}}</p>
+                    <p class="item">{{coinSymbol.toUpperCase()}} / USDT </p>
+<!--                    <a v-if="coinData.links.homepage[0]" class="item" :href="coinData.links.homepage[0]" target="_blank">{{coinData.links.homepage[0].replace('http://','')}}</a>-->
+                </div>
+                <hr>
+                <div class="header">
+                    <button type="button">1d</button>
+                </div>
+                <div class="chart" ref="chartdiv">
+
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+    import * as am4core from "@amcharts/amcharts4/core";
+    import * as am4charts from "@amcharts/amcharts4/charts";
+    import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+    am4core.useTheme(am4themes_animated);
+
+    export default {
+        name: "CoinDetailsModal",
+        props: ['coinId', 'coinSymbol', 'coinHttp'],
+        data() {
+            return {
+                coinData: {},
+            }
+        },
+        methods: {
+            closeModal() {
+                this.$emit('close');
+            },
+            async getCoinDetails() {
+                // await fetch("https://api.coingecko.com/api/v3/coins/" + this.symbol.toLowerCase()).then  (
+                //      res =>  res.json()).then(data => {
+                //     this.coinData = data;
+                // }).catch(e => {
+                //     alert(e)
+                // });
+                console.log("before");
+                const response = await fetch("https://api.coingecko.com/api/v3/coins/" + this.coinId.toLowerCase());
+                const data = await response.json();
+                this.coinData = data;
+                console.log("after");
+            },
+            async getCoinHistoricalData() {
+                let coinHistory= [];
+                await fetch("https://api.binance.com/api/v3/klines?symbol=" + this.coinSymbol.toUpperCase() + "USDT&startTime=1673872038000&endTime=1676550438000&interval=2h").then(res => res.json()).then(data => {
+                    for (let i = 0; i < data.length; i++) {
+                        coinHistory.push({date: data[i][0], name: "", value: data[i][4]})
+                    }
+                }).catch((e) => {
+                    alert(e)
+                });
+                this.createChart(coinHistory);
+            },
+
+            createChart(coinHistory) {
+                let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+                // chart.colors.list = [
+                //     am4core.color("#F9F871")
+                // ];
+                chart.paddingRight = 20;
+
+                let data = [];
+                let visits = 10;
+                for (let i = 1; i < coinHistory.length; i++) {
+                    visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10000);
+                    // data.push({date: new Date(2022, 0, i), name: "name" + i, value: visits});
+                    data.push({
+                        date: coinHistory[i]['date'],
+                        name: "name" + i,
+                        value: coinHistory[i]['value']
+                    });
+                }
+
+                chart.data = data;
+
+                let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+                dateAxis.renderer.grid.template.location = 0;
+                dateAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+
+                let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                valueAxis.tooltip.disabled = true;
+                valueAxis.renderer.minWidth = 35;
+                valueAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+
+                let series = chart.series.push(new am4charts.LineSeries());
+                series.dataFields.dateX = "date";
+                series.dataFields.valueY = "value";
+                series.tooltipText = "{valueY.value}";
+                chart.cursor = new am4charts.XYCursor();
+
+                let scrollbarX = new am4charts.XYChartScrollbar();
+                scrollbarX.series.push(series);
+                chart.scrollbarX = scrollbarX;
+                chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+                this.chart = chart;
+            }
+        },
+        created() {
+            // this.getCoinDetails();
+        },
+
+        mounted() {
+            this.getCoinHistoricalData();
+        },
+        unmounted() {
+            if (this.chart) {
+                this.chart.dispose();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #container {
+        text-align: center;
+    }
+
+    p {
+        color: white;
+    }
+
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+        animation: fadeOut 1s;
+    }
+
+    .modal {
+        width: 50%;
+        padding: 20px;
+        margin: 100px auto;
+        background: #303655;
+        border-radius: 10px;
+        color: white;
+        animation: fadeIn 0.5s;
+    }
+    @keyframes fadeIn {
+        0% { opacity: 0; }
+        100% { opacity: 1; }
+    }
+
+    @keyframes fadeOut {
+        100% {opacity: 1}
+        0% {opacity: 0}
+    }
+
+
+    .close {
+        text-align: right;
+        color: yellow;
+        cursor: pointer;
+        font-size: 20px;
+        transition-duration: 0.5s;
+    }
+
+    .close:hover {
+        color: yellowgreen;
+    }
+
+    .header {
+        display: flex;
+    }
+
+    .item {
+        margin: auto; /* Magic! */
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        transition-duration: 0.5s;
+    }
+
+    a:hover {
+        color: yellowgreen;
+    }
+
+    .chart {
+        width: 100%;
+        height: 500px;
+    }
+
+</style>
